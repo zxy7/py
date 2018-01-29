3bba147652a8895e1e3ad913e4579f0acf5412a7
昝晓玉 <>
2018-01-29 18:27:05 +0800

    save

--- test1.py
+++ test1.py
@@ -45,9 +45,62 @@
 
 # print 'Done'
 
-for i in range(1, 6,2):
-    print i
-    if i==5:
-        break
+# for i in range(1, 6,2):
+#     print i
+#     if i==5:
+#         break
+# else:
+#     print 'The for loop is over'
+
+# def func():
+#     global x
+
+#     print 'x is', x
+#     x = 2
+#     print 'Changed local x to', x
+
+# x = 50
+# func()
+# print 'Value of x is', x
+# def func(a=1, b=5, c=10):
+#     print 'a is', a, 'and b is', b, 'and c is', c
+
+# func(3, 7)
+# func(25, c=24)
+# func(c=50, a=100)
+# func()
+
+
+# def someFunction():
+#     pass
+
+# def printMax(x, y):
+#     '''Prints the maximum of two numbers.
+
+#     The two values must be integers.'''
+#     x = int(x) # convert to integers, if possible
+#     y = int(y)
+#     if x > y:
+#         print x, 'is maximum'
+#     else:
+#         print y, 'is maximum'
+
+# printMax(3, 5)
+# print printMax.__doc__
+
+
+# help(printMax)
+
+import sys
+print dir(sys)
+# print 'The command line arguments are:'
+# for i in sys.argv:
+#     print i
+
+# print '\n\nThe PYTHONPATH is', sys.path, '\n'
+if __name__ == '__main__':
+    print 'This program is being run by itself'
 else:
-    print 'The for loop is over'
+    print 'I am being imported from another module'
+
+print __name__
