6a834684cb88a6d522329b9fd0e7264320781e8f
zxy7 <740410570@qq.com>
2018-02-01 21:17:17 +0800

    save

--- test1.py
+++ test1.py
@@ -144,56 +144,24 @@
 
 
 
-age = 22
-name = 'Swaroop'
-
-print '%s is %d years old' % (name, age)
-print 'Why is %s playing with that python?' % name
+# ag
 
 
-# 'ab' is short for 'a'ddress'b'ook
-
-ab = {       'Swaroop'   : 'swaroopch@byteofpython.info',
-             'Larry'     : 'larry@wall.org',
-             'Matsumoto' : 'matz@ruby-lang.org',
-             'Spammer'   : 'spammer@hotmail.com'
-     }
-
-print "Swaroop's address is %s" % ab['Swaroop']
-
-# Adding a key/value pair
-ab['Guido'] = 'guido@python.org'
-
-# Deleting a key/value pair
-del ab['Spammer']
-
-print '\nThere are %d contacts in the address-book\n' % len(ab)
-for name, address in ab.items():
-    print 'Contact %s at %s' % (name, address)
-
-if 'Guido' in ab: # OR ab.has_key('Guido')
-    print "\nGuido's address is %s" % ab['Guido']
-
-
+print 'Simple Assignment'
 shoplist = ['apple', 'mango', 'carrot', 'banana']
+mylist = shoplist # mylist is just another name pointing to the same object!
 
-# Indexing or 'Subscription' operation
-print 'Item 0 is', shoplist[0]
-print 'Item 1 is', shoplist[1]
-print 'Item 2 is', shoplist[2]
-print 'Item 3 is', shoplist[3]
-print 'Item -1 is', shoplist[-1]
-print 'Item -2 is', shoplist[-2]
+del shoplist[0]
 
-# Slicing on a list
-print 'Item 1 to 3 is', shoplist[1:3]
-print 'Item 2 to end is', shoplist[2:]
-print 'Item 1 to -1 is', shoplist[1:-1]
-print 'Item start to end is', shoplist[:]
+print 'shoplist is', shoplist
+print 'mylist is', mylist
+# notice that both shoplist and mylist both print the same list without
+# the 'apple' confirming that they point to the same object
 
-# Slicing on a string
-name = 'swaroop'
-print 'characters 1 to 3 is', name[1:3]
-print 'characters 2 to end is', name[2:]
-print 'characters 1 to -1 is', name[1:-1]
-print 'characters start to end is', name[:]
+print 'Copy by making a full slice'
+mylist = shoplist[:] # make a copy by doing a full slice
+del mylist[0] # remove first item
+
+print 'shoplist is', shoplist
+print 'mylist is', mylist
+# notice that now the two lists are different
