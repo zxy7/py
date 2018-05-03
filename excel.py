053a4ece256ffe94c64af6775132879473fdacb0
zxy7 <740410570@qq.com>
2018-05-03 10:30:52 +0800

    save

---
+++ excel.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+import  xdrlib ,sys
+import xlrd
+import xlwt
+
+def main():
+    data = xlrd.open_workbook('a.xls')
+    table = data.sheets()[0]
+    nrows = table.nrows #行数
+    try:  
+        # 创建excel文件  
+        filename=xlwt.Workbook()  
+        # 给工作表命名，test  
+        sheet=filename.add_sheet("test")  
+        for rownum in range(1,nrows):
+         row = table.row_values(rownum)
+         if row:
+           print "{title: '"+row[0]+"',"+" key:'"+row[1].lower()+"'}"
+           sheet.write(rownum,0,"{title: '"+row[0]+"',"+" key:'"+row[1].lower()+"'}")
+        filename.save("b.xls")  
+    except Exception,e:  
+        print(str(e)) 
+
+if __name__=="__main__":
+    main()
