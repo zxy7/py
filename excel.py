5b36082762dc8399ff0d7433489ce201b546e67d
zxy7 <740410570@qq.com>
2018-05-05 18:09:39 +0800

    save

--- excel.py
+++ excel.py
@@ -7,6 +7,7 @@ def main():
     data = xlrd.open_workbook('a.xls')
     table = data.sheets()[0]
     nrows = table.nrows #行数
+    print table
     try:  
         # 创建excel文件  
         filename=xlwt.Workbook()  
@@ -15,8 +16,11 @@ def main():
         for rownum in range(1,nrows):
          row = table.row_values(rownum)
          if row:
-           print "{title: '"+row[0]+"',"+" key:'"+row[1].lower()+"'}"
-           sheet.write(rownum,0,"{title: '"+row[0]+"',"+" key:'"+row[1].lower()+"'}")
+           print "{title: '"+row[0]+"',"+" key:'"+row[1].lower()+"',width:100},"
+          #  print "{title: '"+row[0]+"',"+" key:'"+row[1].lower()+"'},"
+          #  print row[1].lower()+":'',"
+           sheet.write(rownum,0,"{title: '"+row[0]+"',"+" key:'"+row[1].lower()+"',width:100}")
+           sheet.write(rownum,1,row[1].lower()+":'',")
         filename.save("b.xls")  
     except Exception,e:  
         print(str(e)) 
